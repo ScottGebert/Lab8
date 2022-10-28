2d2b6f3430c38301d7cdc19f211a6654bdc30fbc
Scott Gebert <scott22.gebert@gmail.com>
2022-10-27 18:11:35 -0600

    Delete City TTD

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -65,7 +65,9 @@ public class CustomList extends ArrayAdapter<City> {
 
 
     public void deleteCity(City city) {
-
+        if (!cities.contains(city))
+            throw new IllegalArgumentException();
+        cities.remove(city);
     }
 
 
